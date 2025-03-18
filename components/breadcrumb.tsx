91895cbafa333c576c46a1cb239cf0df7ef5a0e0
Jovan Lanutan <jovan.lanutan@cwti.local>
2025-03-18 14:16:35 +0800

    Add breadcrumb

---
+++ components/breadcrumb.tsx
@@ -0,0 +1,45 @@
+'use client'
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { matches } from "@/data/match"
+
+export default function Breadcrumb() {
+    const pathname = usePathname();
+    const pathSegments = pathname.split('/').filter(Boolean);
+
+    if (pathSegments.length === 0) return null; // Hide breadcrumb on homepage
+
+    return (
+        <nav className="max-w-7xl mx-auto text-sm text-gray-500 px-4 py-2 md:px-10 md:py-5 md:text-md" >
+            <ol className="flex items-center space-x-2">
+                <li>
+                    <Link href="/" className="text-gray-300 font-bold hover:underline">
+                        Home
+                    </Link>
+                </li>
+                {pathSegments.map((segment, index) => {
+                    const href = `/${pathSegments.slice(0, index + 1).join('/')}`;
+                    const isLast = index === pathSegments.length - 1;
+
+                    const getMatch = matches.find((match) => match.id === segment)
+
+                    const getSegment = getMatch !== undefined ? `${getMatch?.team1} vs ${getMatch?.team2}` : segment
+
+                    return (
+                        <li key={href} className="flex items-center">
+                            <span className="mx-2">/</span>
+                            {isLast ? (
+                                <span className="text-gray-300">{decodeURIComponent(getSegment)}</span>
+                            ) : (
+                                <Link href={href} className="text-gray-300 hover:underline">
+                                    {decodeURIComponent(segment)}
+                                </Link>
+                            )}
+                        </li>
+                    );
+                })}
+            </ol>
+        </nav>
+    );
+}
--- components/header.tsx
+++ components/header.tsx
@@ -4,6 +4,8 @@ import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 
+import Breadcrumb from "./breadcrumb";
+
 function Menu() {
     return (
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2"
@@ -60,6 +62,7 @@ export default function Header() {
                     </nav>
                 )}
             </header>
+            <Breadcrumb />
         </>
     );
 }
