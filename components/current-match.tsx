fb13cc9f8903cbf722225adfa262a3a176955f08
Jovan Lanutan <jovan.lanutan@cwti.local>
2025-03-24 18:52:05 +0800

    Update homepage

--- components/current-match.tsx
+++ components/current-match.tsx
@@ -1,63 +1,71 @@
+import Image from "next/image"
+
 export default function CurrentMatch() {
     return (
         <div className="w-full md:w-[30%] shadow-md">
             <div className="w-full mx-auto bg-black text-white rounded-lg overflow-hidden shadow-lg">
-                <div className="text-center p-4 bg-[#373737] border-b border-[#4b4b4b]">
-                    <p className="text-sm text-gray-400">MAR 13 - 2025</p>
-                    <h2 className="text-lg font-bold">
-                        TEAM 2 WON BY 47 RUNS
+                <div className="text-center p-4 bg-[linear-gradient(to_right,#AC0E10_17%,#D20C14_81%)] border-b border-[#4b4b4b]">
+                    <p className="text-sm text-white">MAR 13 - 2025</p>
+                    <h2 className="text-lg font-bold uppercase">
+                        Chennai super kings won by 4 wickets
                     </h2>
-                    <p className="text-xs text-gray-400">[QUALIFIED]</p>
                 </div>
-
-                <div className="bg-[#282828] p-4 flex flex-col items-center">
-                    <div className="flex items-center space-x-4">
+                <div className="bg-[linear-gradient(to_bottom,#3F3F3F_0%,#282828_100%)]">
+                    <div className="p-4 flex items-center justify-around">
                         <div className="text-center">
-                            <div className="h-12 w-12 rounded-md bg-red-500">
-                                <span className="sr-only">Team banner</span>
+                            <div className="w-[100px] h-[100px]">
+                                <Image src="/img/teams/MIoutline.webp" width={100} height={100} alt="Mumbai Indians" />
                             </div>
-                            <p className="text-xl font-bold">213/4</p>
-                            <p className="text-xs text-gray-400">20 Overs</p>
+                            <div className="leading-[25px] text-center">
+                                <p className="text-[24px] font-bold">155/09</p>
+                                <p className="text-[16px]">20.0 overs</p>
+                            </div>
+                            <p className="mt-4">
+                                IPL 2025
+                            </p>
                         </div>
-                        <span className="text-lg font-bold">VS</span>
+                        <div className="text-[48px] font-bold text-[#FFBF13] leading-[37px]">
+                            vs
+                        </div>
                         <div className="text-center">
-                            <div className="h-12 w-12 rounded-md bg-cyan-500">
-                                <span className="sr-only">Team banner</span>
+                            <div className="w-[100px] h-[100px]">
+                                <Image src="/img/teams/CSKroundbig.webp" width={100} height={100} alt="Chennai Super Kings" />
                             </div>
-                            <p className="text-xl font-bold">166/10</p>
-                            <p className="text-xs text-gray-400">19.2 Overs</p>
+                            <div className="leading-[25px] text-center">
+                                <p className="text-[24px] font-bold">158/06</p>
+                                <p className="text-[16px]">19.1 overs</p>
+                            </div>
+                            <p className="mt-4">
+                                SCORE CARD
+                            </p>
                         </div>
                     </div>
-                    <div className="flex justify-center gap-10 w-full mt-3 text-xs">
-                        <p className="text-gray-400">T20</p>
-                        <p className="text-yellow-400 cursor-pointer">SCORE CARD</p>
+                    <div className="mt-2">
+                        <div className="flex items-center justify-around">
+                            <div className="w-[100px] h-[100px] ml-10">
+                                <Image src="/img/teams/DCoutline.webp" width={100} height={100} alt="Delhi Capitals" />
+                            </div>
+                            <div className="text-[24px] font-bold text-[#FFBF13] leading-[37px]">
+                                24 MAR 2025
+                            </div>
+                            <div className="w-[100px] h-[100px] mr-10">
+                                <Image src="/img/teams/LSGoutline.webp" width={100} height={100} alt="Delhi Capitals" />
+                            </div>
+                        </div>
+                        <div className="flex items-center justify-center text-center py-5">
+                            <div className="leading-[37px] uppercase">
+                                <p className="text-[16px]">Stay connected</p>
+                                <p className="text-[24px] text-[#FFBF13] font-bold">with rcb family</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
-                <div className="bg-[#282828] p-4 flex flex-col items-center">
-                    <div className="flex items-center space-x-4">
-                        <div className="h-12 w-12 rounded-md bg-cyan-500">
-                            <span className="sr-only">Team banner</span>
-                        </div>
-                        <p className="text-xl font-bold text-yellow-400">15 MAR</p>
-                        <div className="h-12 w-12 rounded-md bg-red-500">
-                            <span className="sr-only">Team banner</span>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="bg-[#282828] text-center py-4">
-                    <p className="text-sm">STAY CONNECTED</p>
-                    <p className="text-lg font-bold text-yellow-400">WITH RCB FAMILY</p>
-                    <div className="flex justify-center space-x-4 mt-3">
-                    </div>
-                </div>
-
-                <div className="p-4 flex flex-col space-y-2 bg-[#373737] border-t border-[#4b4b4b]">
-                    <button className="w-full bg-amber-500 py-2 rounded-md font-bold cursor-pointer">
+                <div className="p-4 flex flex-col space-y-2 bg-[linear-gradient(to_bottom,#3F3F3F_0%,#282828_75%)]  border-t border-[#4b4b4b]">
+                    <button className="w-full bg-[#AC0E10] py-2 rounded-md font-bold cursor-pointer">
                         BUY TICKETS
                     </button>
-                    <button className="w-full bg-green-500 py-2 rounded-md font-bold cursor-pointer">
+                    <button className="w-full bg-[#FFBF13] text-black py-2 rounded-md font-bold cursor-pointer">
                         BUY MERCHANDISE
                     </button>
                 </div>
---
+++ data/match.ts
@@ -0,0 +1,263 @@
+export const matches = [
+    {
+        id: 'a8Jk4pNz', date: "March 13, 2025", time: "04:00", team1: "Mumbai Indians", team2: "Gujarat Giants", odds: [1.95, 3.30, 4.20], league: "21ST WPL T20",
+        location: 'Brabourne, CCI, Mumbai',
+        team1Short: "team1",
+        team2Short: "team2",
+        teams: [
+            { banner: '/img/teams/MIoutline.webp', name: "Mumbai Indians", score: '213/04', over: '20.0' },
+            { banner: '/img/teams/GToutline.webp', name: "Gujarat Giants", score: '166/10', over: '19.2' },
+        ]
+    },
+    {
+        id: 's7Lq2xBx', date: "March 15, 2025", time: "01:45", team1: "Mumbai Indians", team2: "Delhi Capitals", odds: [1.90, 3.60, 4.10], league: "22ND WPL T20",
+        location: 'Brabourne, CCI, Mumbai',
+        team1Short: "team1",
+        team2Short: "team2",
+        teams: [
+            { banner: '/img/teams/MIoutline.webp', name: "Mumbai Indians", score: '149/7', over: '20.0' },
+            { banner: '/img/teams/DCoutline.webp', name: "Delhi Capitals", score: '141/09', over: '20.0' },
+        ]
+    },
+    {
+        id: 'X9hYb3Gp', date: "March 22, 2025", time: "04:00", team1: "Kolkata Knight Riders", team2: "Royal Challengers Bengaluru",
+        location: 'Brabourne, CCI, Mumbai',
+        odds: [1.40, 4.75, 7.50], league: "1ST IPL T20 [N]",
+        team1Short: "team1",
+        team2Short: "team2",
+        teams: [
+            { banner: '/img/teams/KKRoutline.webp', name: "Kolkata Knight Riders", score: '174/08', over: '20.0' },
+            { banner: '/img/teams/RCBoutline.webp', name: "Royal Challengers Bengaluru", score: '177/03', over: '16.2' },
+        ]
+    },
+    {
+        id: 'w5Cn0dPz', date: "March 23, 2025", time: "01:45", team1: "Sunrisers Hyderabad", team2: "Rajasthan Royals", odds: [1.90, 3.90, 3.75], league: "2ND IPL T20 D/N",
+        team1Short: "team1",
+        team2Short: "team2",
+        location: 'Brabourne, CCI, Mumbai',
+        teams: [
+            { banner: '/img/teams/SRHoutline.webp', name: "Sunrisers Hyderabad", score: '286/06', over: '20.0' },
+            { banner: '/img/teams/RRoutline.webp', name: "Rajasthan Royals", score: '242/06', over: '16.2' },
+        ]
+    },
+    {
+        id: 'U3Lk8rVw', date: "March 23, 2025", time: "01:45", team1: "Mumbai Indians", team2: "Chennai Super Kings", odds: [1.45, 4.75, 6.50], league: "3RD IPL T20 [N]",
+        team1Short: "team1",
+        team2Short: "team2",
+        location: 'Brabourne, CCI, Mumbai',
+        teams: [
+            { banner: '/img/teams/MIoutline.webp', name: "Mumbai Indians", score: '155/09', over: '20.0' },
+            { banner: '/img/teams/CSKroundbig.webp', name: "Chennai Super Kings", score: '158/06', over: '19.1' },
+        ]
+    },
+    {
+        id: 'A7ZjF4sM', date: "March 01, 2025", time: "01:45", team1: "Team 1", team2: "Team 2", odds: [1.57, 4.33, 5.25], league: "League name",
+        team1Short: "team1",
+        team2Short: "team2",
+        location: 'Brabourne, CCI, Mumbai',
+        teams: [
+            { banner: '/img/teams/MIoutline.webp', name: "Mumbai Indians", score: '155/09', over: '20.0' },
+            { banner: '/img/teams/CSKoutline.webp', name: "Chennai Super Kings", score: '158/06', over: '19.1' },
+        ]
+    },
+    {
+        id: 'Q1oD6nLp', date: "March 01, 2025", time: "04:00", team1: "Team 1", team2: "Team 2", odds: [2.70, 3.75, 2.45], league: "League name",
+        team1Short: "team1",
+        team2Short: "team2",
+        location: 'Brabourne, CCI, Mumbai',
+        teams: [
+            { banner: '/img/teams/MIoutline.webp', name: "Mumbai Indians", score: '155/09', over: '20.0' },
+            { banner: '/img/teams/CSKoutline.webp', name: "Chennai Super Kings", score: '158/06', over: '19.1' },
+        ]
+    },
+    {
+        id: 'd8Fh0TzW', date: "March 01, 2025", time: "04:00", team1: "Team 1", team2: "Team 2", odds: [2.70, 3.75, 2.45], league: "League name",
+        team1Short: "team1",
+        team2Short: "team2",
+        location: 'Brabourne, CCI, Mumbai',
+        teams: [
+            { banner: '/img/teams/MIoutline.webp', name: "Mumbai Indians", score: '155/09', over: '20.0' },
+            { banner: '/img/teams/CSKoutline.webp', name: "Chennai Super Kings", score: '158/06', over: '19.1' },
+        ]
+    },
+    {
+        id: 'V4mK2eZy', date: "March 01, 2025", time: "04:00", team1: "Team 1", team2: "Team 2", odds: [2.70, 3.75, 2.45], league: "League name",
+        team1Short: "team1",
+        team2Short: "team2",
+        location: 'Brabourne, CCI, Mumbai',
+        teams: [
+            { banner: '/img/teams/MIoutline.webp', name: "Mumbai Indians", score: '155/09', over: '20.0' },
+            { banner: '/img/teams/CSKoutline.webp', name: "Chennai Super Kings", score: '158/06', over: '19.1' },
+        ]
+    },
+    {
+        id: 'j3Sg9PoB', date: "March 01, 2025", time: "04:00", team1: "Team 1", team2: "Team 2", odds: [2.70, 3.75, 2.45], league: "League name",
+        team1Short: "team1",
+        team2Short: "team2",
+        location: 'Brabourne, CCI, Mumbai',
+        teams: [
+            { banner: '/img/teams/MIoutline.webp', name: "Mumbai Indians", score: '155/09', over: '20.0' },
+            { banner: '/img/teams/CSKoutline.webp', name: "Chennai Super Kings", score: '158/06', over: '19.1' },
+        ]
+    },
+    {
+        id: 'T6kW8mZp',
+        date: "Mar 25, 2025",
+        time: "10:00",
+        team1: "Team 1",
+        team2: "Team 2",
+        odds: [2.70, 3.75, 2.45],
+        league: "League name",
+        team1Short: "team1",
+        team2Short: "team2",
+        location: 'Brabourne, CCI, Mumbai',
+        teams: [
+            { banner: '/img/teams/MIoutline.webp', name: "Mumbai Indians", score: '155/09', over: '20.0' },
+            { banner: '/img/teams/CSKoutline.webp', name: "Chennai Super Kings", score: '158/06', over: '19.1' },
+        ]
+    },
+    {
+        id: 'r1Vx7AqY',
+        date: "Mar 25, 2025",
+        team1: "Team 1",
+        team2: "Team 2",
+        league: "League name",
+        odds: [2.70, 3.75, 2.45],
+        time: "10:00",
+        team1Short: "team1",
+        team2Short: "team2",
+        location: 'Brabourne, CCI, Mumbai',
+        teams: [
+            { banner: '/img/teams/MIoutline.webp', name: "Mumbai Indians", score: '155/09', over: '20.0' },
+            { banner: '/img/teams/CSKoutline.webp', name: "Chennai Super Kings", score: '158/06', over: '19.1' },
+        ]
+    },
+    {
+        id: 'G2oQ9wLz',
+        team1: "Team 1",
+        team2: "Team 2",
+        league: "League name",
+        date: "Mar 25, 2025",
+        time: "10:00",
+        team1Short: "team1",
+        team2Short: "team2",
+        odds: [2.70, 3.75, 2.45],
+        location: 'Brabourne, CCI, Mumbai',
+        teams: [
+            { banner: '/img/teams/MIoutline.webp', name: "Mumbai Indians", score: '155/09', over: '20.0' },
+            { banner: '/img/teams/CSKoutline.webp', name: "Chennai Super Kings", score: '158/06', over: '19.1' },
+        ]
+    },
+    {
+        id: 'h5BfX3pJ',
+        team1: "Team 1",
+        team2: "Team 2",
+        league: "League name",
+        date: "Mar 25, 2025",
+        time: "10:00",
+        team1Short: "team1",
+        team2Short: "team2",
+        odds: [2.70, 3.75, 2.45],
+        location: 'Brabourne, CCI, Mumbai',
+        teams: [
+            { banner: '/img/teams/MIoutline.webp', name: "Mumbai Indians", score: '155/09', over: '20.0' },
+            { banner: '/img/teams/CSKoutline.webp', name: "Chennai Super Kings", score: '158/06', over: '19.1' },
+        ]
+    },
+    {
+        id: 'N7aD0kLw',
+        team1: "Team 1",
+        team2: "Team 2",
+        league: "League name",
+        date: "Mar 25, 2025",
+        time: "10:00",
+        team1Short: "team1",
+        team2Short: "team2",
+        odds: [2.70, 3.75, 2.45],
+        location: 'Brabourne, CCI, Mumbai',
+        teams: [
+            { banner: '/img/teams/MIoutline.webp', name: "Mumbai Indians", score: '155/09', over: '20.0' },
+            { banner: '/img/teams/CSKoutline.webp', name: "Chennai Super Kings", score: '158/06', over: '19.1' },
+        ]
+    },
+    {
+        id: 'P8zC4vHr',
+        team1: "Team 1",
+        team2: "Team 2",
+        league: "League name",
+        date: "Mar 25, 2025",
+        time: "10:00",
+        team1Short: "team1",
+        team2Short: "team2",
+        odds: [2.70, 3.75, 2.45],
+        location: 'Brabourne, CCI, Mumbai',
+        teams: [
+            { banner: '/img/teams/MIoutline.webp', name: "Mumbai Indians", score: '155/09', over: '20.0' },
+            { banner: '/img/teams/CSKoutline.webp', name: "Chennai Super Kings", score: '158/06', over: '19.1' },
+        ]
+    },
+    {
+        id: 's9QjT1Fv',
+        team1: "Team 1",
+        team2: "Team 2",
+        league: "League name",
+        date: "Mar 25, 2025",
+        time: "10:00",
+        team1Short: "team1",
+        team2Short: "team2",
+        odds: [2.70, 3.75, 2.45],
+        location: 'Brabourne, CCI, Mumbai',
+        teams: [
+            { banner: '/img/teams/MIoutline.webp', name: "Mumbai Indians", score: '155/09', over: '20.0' },
+            { banner: '/img/teams/CSKoutline.webp', name: "Chennai Super Kings", score: '158/06', over: '19.1' },
+        ]
+    },
+    {
+        id: 'Z3mW2oUe',
+        team1: "Team 1",
+        team2: "Team 2",
+        league: "League name",
+        date: "Mar 25, 2025",
+        time: "10:00",
+        team1Short: "team1",
+        team2Short: "team2",
+        odds: [2.70, 3.75, 2.45],
+        location: 'Brabourne, CCI, Mumbai',
+        teams: [
+            { banner: '/img/teams/MIoutline.webp', name: "Mumbai Indians", score: '155/09', over: '20.0' },
+            { banner: '/img/teams/CSKoutline.webp', name: "Chennai Super Kings", score: '158/06', over: '19.1' },
+        ]
+    },
+    {
+        id: 'd5XsR8Gj',
+        team1: "Team 1",
+        team2: "Team 2",
+        league: "League name",
+        date: "Mar 25, 2025",
+        time: "10:00",
+        team1Short: "team1",
+        team2Short: "team2",
+        odds: [2.70, 3.75, 2.45],
+        location: 'Brabourne, CCI, Mumbai',
+        teams: [
+            { banner: '/img/teams/MIoutline.webp', name: "Mumbai Indians", score: '155/09', over: '20.0' },
+            { banner: '/img/teams/CSKoutline.webp', name: "Chennai Super Kings", score: '158/06', over: '19.1' },
+        ]
+    },
+    {
+        id: 'F4nK9bYh',
+        team1: "Team 1",
+        team2: "Team 2",
+        league: "League name",
+        date: "Mar 25, 2025",
+        time: "10:00",
+        team1Short: "team1",
+        team2Short: "team2",
+        odds: [2.70, 3.75, 2.45],
+        location: 'Brabourne, CCI, Mumbai',
+        teams: [
+            { banner: '/img/teams/MIoutline.webp', name: "Mumbai Indians", score: '155/09', over: '20.0' },
+            { banner: '/img/teams/CSKoutline.webp', name: "Chennai Super Kings", score: '158/06', over: '19.1' },
+        ]
+    },
+];
