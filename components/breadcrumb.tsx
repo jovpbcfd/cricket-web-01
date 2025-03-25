1ae7c496014fd9f2e058bca3a5f1f7d826e8ff52
Jovan Lanutan <jovan.lanutan@cwti.local>
2025-03-25 16:28:49 +0800

    Update matches UI

--- components/breadcrumb.tsx
+++ components/breadcrumb.tsx
@@ -14,7 +14,7 @@ export default function Breadcrumb() {
         <nav className="max-w-7xl mx-auto text-sm text-gray-500 px-4 py-2 md:px-10 md:py-5 md:text-md" >
             <ol className="flex items-center space-x-2">
                 <li>
-                    <Link href="/" className="text-gray-300 font-bold hover:underline">
+                    <Link href="/" className="text-[#565656] font-bold hover:underline">
                         Home
                     </Link>
                 </li>
@@ -30,9 +30,9 @@ export default function Breadcrumb() {
                         <li key={href} className="flex items-center">
                             <span className="mx-2">/</span>
                             {isLast ? (
-                                <span className="text-gray-300">{decodeURIComponent(getSegment)}</span>
+                                <span className="text-[#565656]">{decodeURIComponent(getSegment)}</span>
                             ) : (
-                                <Link href={href} className="text-gray-300 hover:underline">
+                                <Link href={href} className="text-[#565656] hover:underline">
                                     {decodeURIComponent(segment)}
                                 </Link>
                             )}
